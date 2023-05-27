9e412220a05e4f69d8887418b9ecb8d6c02528c9
Logan Lawson <130174775+lawsonlo1@users.noreply.github.com>
2023-05-26 20:45:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 #mentorapp.py
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+# Connect to the MongoDB Atlas cluster
+client = MongoClient('mongodb+srv://lawsonlo1:<Gothampearl123>@gothamdata.wgyabvu.mongodb.net/?retryWrites=true&w=majority')
+db = client['gothamdata']
+emails_collection = db['emails']
+
+# Existing route for the landing page
 @app.route('/')
 def landing_page():
     return render_template('index.html')
 
+# New route to handle form submission
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+    email = request.form['email']  # Get the submitted email from the form
+    # Store the email in the MongoDB collection
+    emails_collection.insert_one({'email': email})
+    return 'Thank you for subscribing!'
+
 if __name__ == '__main__':
     app.run(port=5000)
-
